d513e55dbddee8fc235a6d38f65e16204aa54c7d
shannon112 <b03505011@ntu.edu.tw>
2019-10-21 00:49:34 +0800

    update upload version

---
+++ hw3/r07921001_hw3/src/db/dbCmd.cpp
@@ -0,0 +1,441 @@
+/****************************************************************************
+  FileName     [ dbCmd.cpp ]
+  PackageName  [ db ]
+  Synopsis     [ Define database commands ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2015-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <cmath>
+#include "util.h"
+#include "dbCmd.h"
+#include "dbJson.h"
+
+// Global variable
+DBJson dbjson;
+
+// registered Quit, HIStory, HELp, DOfile commands
+bool
+initDbCmd()
+{
+   // TODO
+   if (!(cmdMgr->regCmd("DBAppendCmd", 4, new DBAppendCmd) &&
+         cmdMgr->regCmd("DBAveCmd", 4, new DBAveCmd) &&
+         cmdMgr->regCmd("DBCountCmd", 3, new DBCountCmd) &&
+         cmdMgr->regCmd("DBMaxCmd", 4, new DBMaxCmd) &&
+         cmdMgr->regCmd("DBMinCmd", 4, new DBMinCmd) &&
+         cmdMgr->regCmd("DBPrintCmd", 3, new DBPrintCmd) &&
+         cmdMgr->regCmd("DBReadCmd", 3, new DBReadCmd) &&
+         cmdMgr->regCmd("DBSortCmd", 4, new DBSortCmd) &&
+         cmdMgr->regCmd("DBSumCmd", 4, new DBSumCmd)
+      )) {
+      cerr << "Registering \"init\" DB commands fails... exiting" << endl;
+      return false;
+   }
+   return true;
+}
+
+//----------------------------------------------------------------------
+//    DBAPpend <(string key)><(int value)>
+//----------------------------------------------------------------------
+CmdExecStatus
+DBAppendCmd::exec(const string& option)
+{
+   // TODO
+   // check option
+   vector<string> options;
+
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexOptions(option, options))
+      return CMD_EXEC_ERROR;
+
+   if ((options.empty())||(options.size()==1))
+      return CmdExec::errorOption(CMD_OPT_MISSING, "");
+
+   if (options.size()>2)
+      return CmdExec::errorOption(CMD_OPT_EXTRA, options[2]);
+
+   //check key
+   string key;
+   if (isValidVarName(options[0])) key = options[0];
+   else return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[0]);
+
+   //check value
+   if ((!isdigit(options[1][0]))&&(options[1][0]!='-')) return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[1]);
+   for (size_t i=1; i<options[1].length(); i++)
+   {
+     if (!isdigit(options[1][i])) return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[1]);
+   }
+   int value = atoi(options[1].c_str());
+
+   //check if key exist, and append
+   DBJsonElem jsonElem(key,value);
+   if (!(dbjson.add(jsonElem))){
+     cerr<<"Error: Element with key \""<<key<<"\" already exists!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+   return CMD_EXEC_DONE;
+}
+
+void
+DBAppendCmd::usage(ostream& os) const
+{
+   os << "Usage: DBAPpend <(string key)><(int value)>" << endl;
+}
+
+void
+DBAppendCmd::help() const
+{
+   cout << setw(15) << left << "DBAPpend: "
+        << "append an JSON element (key-value pair) to the end of DB" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBAVerage
+//----------------------------------------------------------------------
+CmdExecStatus
+DBAveCmd::exec(const string& option)
+{
+   // check option
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexNoOption(option))
+      return CMD_EXEC_ERROR;
+
+   float a = dbjson.ave();
+   if (isnan(a)) {
+      cerr << "Error: The average of the DB is nan." << endl;
+      return CMD_EXEC_ERROR;
+   }
+   ios_base::fmtflags origFlags = cout.flags();
+   cout << "The average of the DB is " << fixed
+        << setprecision(2) << a << ".\n";
+   cout.flags(origFlags);
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBAveCmd::usage(ostream& os) const
+{
+   os << "Usage: DBAVerage" << endl;
+}
+
+void
+DBAveCmd::help() const
+{
+   cout << setw(15) << left << "DBAVerage: "
+        << "compute the average of the DB" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBCount
+//----------------------------------------------------------------------
+CmdExecStatus
+DBCountCmd::exec(const string& option)
+{
+   // check option
+   if (!CmdExec::lexNoOption(option))
+      return CMD_EXEC_ERROR;
+
+   size_t n = dbjson.size();
+   if (n > 1)
+      cout << "There are " << n << " JSON elements in DB." << endl;
+   else if (n == 1)
+      cout << "There is 1 JSON element in DB." << endl;
+   else
+      cout << "There is no JSON element in DB." << endl;
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBCountCmd::usage(ostream& os) const
+{
+   os << "Usage: DBCount" << endl;
+}
+
+void
+DBCountCmd::help() const
+{
+   cout << setw(15) << left << "DBCount: "
+        << "report the number of JSON elements in the DB" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBMAx
+//----------------------------------------------------------------------
+CmdExecStatus
+DBMaxCmd::exec(const string& option)
+{
+   // check option
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexNoOption(option))
+      return CMD_EXEC_ERROR;
+
+   size_t maxI;
+   int maxN = dbjson.max(maxI);
+   if (maxN == INT_MIN) {
+      cerr << "Error: The max JSON element is nan." << endl;
+      return CMD_EXEC_ERROR;
+   }
+   cout << "The max JSON element is { " << dbjson[maxI] << " }." << endl;
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBMaxCmd::usage(ostream& os) const
+{
+   os << "Usage: DBMAx" << endl;
+}
+
+void
+DBMaxCmd::help() const
+{
+   cout << setw(15) << left << "DBMAx: "
+        << "report the maximum JSON element" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBMIn
+//----------------------------------------------------------------------
+CmdExecStatus
+DBMinCmd::exec(const string& option)
+{
+   // check option
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexNoOption(option))
+      return CMD_EXEC_ERROR;
+
+   size_t minI;
+   int minN = dbjson.min(minI);
+   if (minN == INT_MAX) {
+      cerr << "Error: The min JSON element is nan." << endl;
+      return CMD_EXEC_ERROR;
+   }
+   cout << "The min JSON element is { " << dbjson[minI] << " }." << endl;
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBMinCmd::usage(ostream& os) const
+{
+   os << "Usage: DBMIn" << endl;
+}
+
+void
+DBMinCmd::help() const
+{
+   cout << setw(15) << left << "DBMIn: "
+        << "report the minimum JSON element" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBPrint [(string key)]
+//----------------------------------------------------------------------
+CmdExecStatus
+DBPrintCmd::exec(const string& option)
+{
+   // TODO...
+   // check option
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   string token;
+   if (!CmdExec::lexSingleOption(option, token, true))
+     return CMD_EXEC_ERROR;
+
+   if (token.empty()){
+     cout << dbjson;
+     cout << "Total JSON elements: " << dbjson.size() << endl;
+   }
+   else{
+     for (size_t i = 0; i<=dbjson.size(); i++){
+        if (i==dbjson.size()) cout<<"Error: No JSON element with key \""<<token<<"\" is found."<<endl;
+        if (dbjson[i].key()==token){
+          cout<<"{ "<<dbjson[i]<<" }"<<endl;
+          break;
+        }
+     }
+   }
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBPrintCmd::usage(ostream& os) const
+{
+   os << "DBPrint [(string key)]" << endl;
+}
+
+void
+DBPrintCmd::help() const
+{
+   cout << setw(15) << left << "DBPrint: "
+        << "print the JSON element(s) in the DB" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBRead <(string jsonFile)> [-Replace]
+//----------------------------------------------------------------------
+CmdExecStatus
+DBReadCmd::exec(const string& option)
+{
+   // check option
+   vector<string> options;
+   if (!CmdExec::lexOptions(option, options))
+      return CMD_EXEC_ERROR;
+
+   if (options.empty())
+      return CmdExec::errorOption(CMD_OPT_MISSING, "");
+
+   bool doReplace = false;
+   string fileName;
+   for (size_t i = 0, n = options.size(); i < n; ++i) {
+      if (myStrNCmp("-Replace", options[i], 2) == 0) {
+         if (doReplace) return CmdExec::errorOption(CMD_OPT_EXTRA,options[i]);
+         doReplace = true;
+      }
+      else {
+         if (fileName.size())
+            return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+         fileName = options[i];
+      }
+   }
+
+   ifstream ifs(fileName.c_str());
+   if (!ifs) {
+      cerr << "Error: \"" << fileName << "\" does not exist!!" << endl;
+      return CMD_EXEC_ERROR;
+   }
+
+   if (dbjson) {
+      if (!doReplace) {
+         cerr << "Error: DB exists. Use \"-Replace\" option for "
+              << "replacement.\n";
+         return CMD_EXEC_ERROR;
+      }
+      cout << "DB is replaced..." << endl;
+      dbjson.reset();
+   }
+//   if (!(ifs >> dbtbl)) return CMD_EXEC_ERROR;
+   ifs >> dbjson;
+   cout << "\"" << fileName << "\" was read in successfully." << endl;
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBReadCmd::usage(ostream& os) const
+{
+   os << "Usage: DBRead <(string jsonFile)> [-Replace]" << endl;
+}
+
+void
+DBReadCmd::help() const
+{
+   cout << setw(15) << left << "DBRead: "
+        << "read data from .json file" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBSOrt <-Key | -Value>
+//----------------------------------------------------------------------
+CmdExecStatus
+DBSortCmd::exec(const string& option)
+{
+   // check option
+   string token;
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexSingleOption(option, token, false))
+      return CMD_EXEC_ERROR;
+
+   if (myStrNCmp("-Key", token, 2) == 0) dbjson.sort(DBSortKey());
+   else if (myStrNCmp("-Value", token, 2) == 0) dbjson.sort(DBSortValue());
+   else return CmdExec::errorOption(CMD_OPT_ILLEGAL, token);
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBSortCmd::usage(ostream& os) const
+{
+   os << "Usage: DBSOrt <-Key | -Value>" << endl;
+}
+
+void
+DBSortCmd::help() const
+{
+   cout << setw(15) << left << "DBSOrt: "
+        << "sort the JSON object by key or value" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    DBSUm
+//----------------------------------------------------------------------
+CmdExecStatus
+DBSumCmd::exec(const string& option)
+{
+   // check option
+   if (dbjson.empty()){
+     cerr<<"Error: DB is not created yet!!"<<endl;
+     return CMD_EXEC_ERROR;
+   }
+
+   if (!CmdExec::lexNoOption(option))
+      return CMD_EXEC_ERROR;
+
+   if (dbjson.empty()) {
+      cerr << "Error: The sum of the DB is nan." << endl;
+      return CMD_EXEC_ERROR;
+   }
+   cout << "The sum of the DB is " << dbjson.sum() << "." << endl;
+
+   return CMD_EXEC_DONE;
+}
+
+void
+DBSumCmd::usage(ostream& os) const
+{
+   os << "Usage: DBSUm" << endl;
+}
+
+void
+DBSumCmd::help() const
+{
+   cout << setw(15) << left << "DBSUm: "
+        << "compute the summation of the DB" << endl;
+}
---
+++ hw3/r07921001_hw3/src/db/dbJson.cpp
@@ -0,0 +1,173 @@
+/****************************************************************************
+  FileName     [ dbJson.cpp ]
+  PackageName  [ db ]
+  Synopsis     [ Define database Json member functions ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2015-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include "dbJson.h"
+#include "util.h"
+#include <regex>
+
+using namespace std;
+
+/*****************************************/
+/*          Global Functions             */
+/*****************************************/
+// print json element
+ostream&
+operator << (ostream& os, const DBJsonElem& j)
+{
+   os << "\"" << j._key << "\" : " << j._value;
+   return os;
+}
+
+// TODO:read into json databse
+istream& operator >> (istream& is, DBJson& j)
+{
+   // to read in data from Json file and store them in a DB
+   // - You can assume the input file is with correct JSON file format
+   // - NO NEED to handle error file format
+   assert(j._obj.empty());
+
+   // regex pattern
+   regex reg("[ \t\b\r\n]*\"([^ \t\b\r\n]+)\"[ \t\b\r\n]*[ |\t]:[ |\t][ \t\b\r\n]*(-[0-9]*|[0-9]*).*");
+   smatch m;
+
+   // read input file
+   std::string line;
+   while (getline(is, line)){
+     if(regex_match(line, m, reg)) {
+       // save as JsonElem type
+       string key = m[1];
+       string value = m[2];
+       DBJsonElem jsonElem(key,atoi(value.c_str()));
+       // push to database
+       j._obj.push_back(jsonElem);
+     }
+   }
+   return is;
+}
+
+// TODO:print out json database content
+ostream& operator << (ostream& os, const DBJson& j)
+{
+   cout<<"{"<<endl;
+   for(size_t i=0; i<j.size(); i++){
+     if( i == j.size()-1 ) cout<<"  "<<j._obj[i]<<endl;
+     else cout<<"  "<<j._obj[i]<<","<<endl;
+   }
+   cout<<"}"<<endl;
+   return os;
+}
+
+/**********************************************/
+/*   Member Functions for class DBJsonElem    */
+/**********************************************/
+/*****************************************/
+/*   Member Functions for class DBJson   */
+/*****************************************/
+void
+DBJson::reset()
+{
+  // TODO
+   vector<DBJsonElem>().swap(_obj);
+}
+
+// return false if key is repeated
+bool
+DBJson::add(const DBJsonElem& elm)
+{
+   // TODO
+   for (size_t i = 0; i<this->size(); i++){
+     if(_obj[i].key() == elm.key()) return false;
+   }
+   _obj.push_back(elm);
+   return true;
+}
+
+// return NAN if DBJson is empty
+float
+DBJson::ave() const
+{
+   // TODO
+   return (float)(this->sum()) / this->size();
+}
+
+// If DBJson is empty, set idx to size() and return INT_MIN
+int
+DBJson::max(size_t& idx) const
+{
+   // TODO
+   if (this->empty()){
+     idx = this->size();
+     return INT_MIN;
+   }
+   else{
+     int max_value = INT_MIN;
+     for(size_t i=0; i<this->size(); i++){
+       if( _obj[i].value() > max_value ){
+         max_value = _obj[i].value();
+         idx = i;
+       }
+     }
+     return max_value;
+   }
+}
+
+// If DBJson is empty, set idx to size() and return INT_MAX
+int
+DBJson::min(size_t& idx) const
+{
+   // TODO
+   if (this->empty()){
+     idx = this->size();
+     return INT_MAX;
+   }
+   else{
+     int min_value = INT_MAX;
+     for(size_t i=0; i<this->size(); i++){
+       if( _obj[i].value() < min_value ){
+         min_value = _obj[i].value();
+         idx = i;
+       }
+     }
+     return min_value;
+   }
+}
+
+void
+DBJson::sort(const DBSortKey& s)
+{
+   // Sort the data according to the order of columns in 's'
+   ::sort(_obj.begin(), _obj.end(), s);
+}
+
+void
+DBJson::sort(const DBSortValue& s)
+{
+   // Sort the data according to the order of columns in 's'
+   ::sort(_obj.begin(), _obj.end(), s);
+}
+
+// return 0 if empty
+int
+DBJson::sum() const
+{
+   // TODO
+   int sum = 0;
+   for(size_t i=0; i<this->size(); i++){
+     sum += _obj[i].value();
+   }
+   return sum;
+}
