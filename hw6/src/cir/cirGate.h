2b0376d7bb51872c8e94223b1ed52f57ccc4161b
shannon112 <b03505011@ntu.edu.tw>
2019-12-03 16:43:30 +0800

    define version of cirgate class

--- hw6/src/cir/cirGate.h
+++ hw6/src/cir/cirGate.h
@@ -13,10 +13,9 @@
 #include <vector>
 #include <iostream>
 #include "cirDef.h"
+#include <map>
 
 using namespace std;
-
-class CirGate;
 
 //------------------------------------------------------------------------
 //   Define classes
@@ -31,17 +30,46 @@ public:
    // Basic access methods
    string getTypeStr() const { return ""; }
    unsigned getLineNo() const { return 0; }
+   virtual void setFanin(const int&, const unsigned&) = 0;
+   virtual void setInvPhase(const int&, const bool&) = 0;
 
    // Printing functions
    virtual void printGate() const = 0;
+   virtual void printGateId() const = 0;
+
    void reportGate() const;
    void reportFanin(int level) const;
    void reportFanout(int level) const;
 
 private:
-
 protected:
 
 };
+
+#define CirGateClass(T)                                 \
+class T: public CirGate                                 \
+{                                                       \
+public:                                                 \
+   T(int gateID, int lineNo):_gateID(gateID),_lineNo(lineNo){\
+     _faninList = new unsigned[2];                      \
+     _invPhase = new bool[2];                           \
+     _cirgateList = new CirGate*[2];                    \
+   }                                                    \
+   ~T() {}                                              \
+   void printGate() const {cout<<"print"<<endl;return;} \
+   void printGateId() const {cout<<_gateID;return;}     \
+   void setFanin(const int&, const unsigned&){return;}  \
+   void setInvPhase(const int&, const bool&){return;}   \
+private:                                                \
+   unsigned _gateID;                                    \
+   unsigned _lineNo;                                    \
+   unsigned* _faninList;                                \
+   bool* _invPhase;                                     \
+   CirGate** _cirgateList;                              \
+}
+
+CirGateClass(CirPiGate);
+CirGateClass(CirPoGate);
+CirGateClass(CirAigGate);
 
 #endif // CIR_GATE_H
